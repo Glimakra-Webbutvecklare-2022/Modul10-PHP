33aaf378e0762fbf1b195e6829d6926b676ee391
Anders Sjunnesson <anders.sj@glimnet.se>
2024-01-18 14:01:01 +0100

    languages user foreign key

--- app/public/_models/Language.php
+++ app/public/_models/Language.php
@@ -6,10 +6,38 @@ class Language extends Database
     {
         parent::__construct();
     }
-
-    public function get_all()
+    
+    /**
+     * get_all
+     *
+     * @param  string $order_by  column "language" or "language_type"
+     * @param  string $order "ASC" or "DESC" (DESC default)
+     * @return array
+     */
+    public function get_all($order_by, $order)
     {
-        $sql = "SELECT * FROM languages";
+        // order_by - known column name
+        $columns = array("language", "language_type");
+        if (in_array($order_by, $columns)) {
+            $sql_order_by = "ORDER BY $order_by";
+        } else {
+            $sql_order_by = "ORDER BY language";
+        }
+
+        $orders = array("ASC", "DESC");
+        if (in_array($order, $orders)) {
+            $sql_order = " " . $order;
+        } else {
+            $sql_order = "";
+        }
+
+        // ternary operator - if-else oneliner...
+        $order = $order === "DESC" ? " DESC" : " ASC";
+
+
+        $sql = "SELECT * FROM languages ";
+        $sql .= $sql_order_by;
+        $sql .= $order;
         $stmt = $this->db->prepare($sql);
         $stmt->execute();
 
--- app/public/test.php
+++ app/public/test.php
@@ -2,12 +2,13 @@
 
 include_once "_models/Database.php";
 include_once "_models/Language.php";
+include_once "_includes/global-functions.php";
 
 $database = new Database();
 $language = new Language();
 
-$rows = $language->get_all();
+$rows = $language->get_all("language", "ASC");
 
-print_r($rows);
+print_r2($rows);
 
 ?>
